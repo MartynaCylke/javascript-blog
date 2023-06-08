a7fe651b582c2b65bd420a0d6b834dc11b48191b
Martyna <pugdogxxx@gmail.com>
2023-06-08 16:04:33 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,75 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
   
-    /* add class 'active' to the clicked link */
+    /*[IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
   
-    /* remove class 'active' from all articles */
+    /*[DONE] remove class 'active' from all articles */
     const activeLinks = document.querySelectorAll('.articles a.active');
 
 for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
   
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
   
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle:', targetArticle);
   
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
+
+    const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  /* remove contents of titleList */
+  /* ... */
+
+  /* find all the articles and save them to variable: articles */
+  /* ... */
+
+  let html = '';
+
+  for(let article of articles){
+    /* get the article id */
+    /* ... */
+
+    /* find the title element */
+    /* ... */
+
+    /* get the title from the title element */
+    /* ... */
+
+    /* create HTML of the link */
+    /* ... */
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
